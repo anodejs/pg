6a9065337245ffa15b98e5c052744e4c7ecbdcd2
Yosef Dinerstein <yosefd@microsoft.com>
2012-07-16 16:16:21 +0300

    first commit

--- yosefd/demo1/index.js
+++ yosefd/demo1/index.js
@@ -1,21 +1,15 @@
 var express = require('express');
 
-console.info('try2 starting');
+console.info('demo starting');
 
 var srv = express.createServer();
-
 srv.listen(process.env.PORT || 5000);
 
 var count = 0;
 
 srv.get('/', function(req, res) {
-  console.info('home', count++);
+  console.info('root count:', count++);
   res.send('Hello from anode!', 200);
 });
 
-srv.get('/bad/?', function(req, res) {
-  console.error('very bad');
-  res.send('Forbidden zone', 403);
-});
-
-console.info('try2 started');
+console.info('demo started');
