98853bd1bb81194e55422361e60ffc3e79634ac2
Yosef Dinerstein <yosefd@microsoft.com>
2012-07-17 22:39:23 +0300

    Update master

--- juddster/test/test/index.js
+++ juddster/test/test/index.js
@@ -1,2 +1,2 @@
 var rinuts = require('rinuts-nodeunitDriver'); // use the rinuts nodeunit driver
-rinuts.listen(require('./tests'), process.env.PORT || 7000); // bind the rinuts server to the http endpoint
+rinuts.listen(require('./test'), process.env.PORT || 7000); // bind the rinuts server to the http endpoint
