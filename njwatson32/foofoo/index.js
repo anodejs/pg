5089840f0209488edcd6f21876a65bdfbe4cbea6
Nick Watson <njwatson32@gmail.com>
2012-07-20 14:56:26 -0700

    my first anode app

---
+++ njwatson32/foofoo/index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function(req, res) {
+  console.log('new request from ' + req.url);
+  console.warn('this is a warning');
+  console.error('this is an error');
+  res.end('yo yo yo from anode');
+}).listen(process.env.PORT || 5000);
+console.log('info: foofoo started at', new Date());
