eda26c63d611100bc8848f29ef6d4083e554744f
Nick Watson <njwatson32@gmail.com>
2012-07-26 16:28:17 -0700

    added update account

--- njwatson32/accounts/index.js
+++ njwatson32/accounts/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 var server = express.createServer();
 var port = 12345;
@@ -12,7 +13,8 @@ function addAccount(req) {
     return null;
   else {
     var acct = req.body;
-    acct.status = 'enabled';
+    acct.status = 'active';
+    //acct.joinDate = new Date();
     accounts[req.params.id] = acct;
     return acct;
   }
@@ -23,7 +25,11 @@ function getAccount(id) {
 }
 
 function updateAccount(req) {
-
+  if (accounts[req.params.id] == undefined)
+    return null;
+  req.body.status = 'active';
+  accounts[req.params.id] = req.body;
+  return req.body;
 }
 
 server.configure(function() {
@@ -34,12 +40,53 @@ server.configure(function() {
   server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+function htmlStringify(obj) {
+  var tab = '&nbsp;&nbsp;&nbsp;&nbsp;';
+  var str = '{<br />';  
+  for (var prop in obj) {
+    str += tab + prop + ': ' + obj[prop] + '<br />';
+  }
+  return str + '}';
+}
+
 server.get('/:id/Account', function(req, res) {
   var acct = getAccount(req.params.id);
-  if (acct == undefined)
-    res.send("Account not found!");
+  if (acct == null)
+    res.send("Account not found!", 404);
   else
     res.send(acct);
+});
+
+server.get(':id/Account/home', function(req, res) {
+  
+});
+
+server.get('/:id/Account/edit', function(req, res) {
+  var acct = getAccount(req.params.id);
+  if (acct == null)
+    res.send("Account not found!", 404);
+  else {
+    fs.readFile(path.join(__dirname, 'edit_account.html'), function(err, data) {
+      if (err) {
+        res.send(err, 500);
+        return;
+      }
+      var nameHtml = 'First Name: <input type="text" id="fst" value="__FST__"/> Last Name: <input type="text" id="lst" value="__LST__"/>';
+      if (acct.cmp != '')
+        nameHtml = 'Company Name: <input type="text" id="cmp" value="__CMP__"/>'
+      data = data.toString().replace('__NAME_HTML__', nameHtml)
+        .replace('__ACCT_OBJ__', htmlStringify(acct))
+        .replace('__ACCT_ID__', req.params.id)
+        .replace('__FST__', acct.fst)
+        .replace('__LST__', acct.lst)
+        .replace('__CMP__', acct.cmp)
+        .replace('__LIVE_ID__', acct.liveId)
+        .replace('__PUID__', acct.puid)
+        .replace('__LANG__', acct.lang)
+        .replace('__C__', acct.country);
+      res.send(data);
+    });
+  }
 });
 
 server.post('/:id/Account', function(req, res) {
@@ -52,18 +99,18 @@ server.post('/:id/Account', function(req, res) {
   }
 });
 
+server.put('/:id/Account', function(req, res) {
+  var acct = updateAccount(req);
+  if (acct == null)
+    res.send('Account not found!', 404);
+  else
+    res.send(acct);
+});
+
 server.post('/signup/submit', function(req, res) {
   console.log(req);
   res.send('<data><result>0</result><aid>' + newGuid() + '</aid></data>',
            { 'Content-Type': 'application/xml' }, 201);
-});
-
-server.put('/:id/Account', function(req, res) {
-  var acct = updateAccount(req);
-  if (acct == null)
-    res.send('Account not found!');
-  else
-    res.send(acct);
 });
 
 server.listen(process.env.PORT || port, function() {
