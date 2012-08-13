0dd8438f6f78b51ca326714da9b2dceeb5d4adb2
Lilach Perry <t-lperry@microsoft.com>
2012-08-13 21:15:35 +0300

    testing

--- lilachp/foofoo/index.js
+++ lilachp/foofoo/index.js
@@ -3,6 +3,6 @@ http.createServer(function(req, res) {
  console.log('new request from ' + req.url);
  console.warn('this is a warning');
  console.error('this is an error');
- res.end('yo yo yo from anode');
+ res.end('yo yo yo from anode!'); 
 }).listen(process.env.PORT || 5000);
-console.log('info: foofoo started at', new Date());
+console.log('info1: foofoo started at', new Date());
