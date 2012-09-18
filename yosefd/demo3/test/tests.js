9010ae5ff2a0f319959736761615176cc13882a4
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-18 11:50:01 +0300

    debug tests

--- yosefd/demo3/test/tests.js
+++ yosefd/demo3/test/tests.js
@@ -1,14 +1,25 @@
 var request = require('request');
 
-exports.test1 = function(test) {
+function target(test) {
   var protocol = (test.context && test.context.protocol) || 'http://';
   var host = (test.context && test.context.host) || 'localhost:5000';
-  var target = protocol + host;
+  return protocol + host;
+}
 
-  request(target, function(err, res) {
+exports.test1 = function(test) {
+  request(target(test), function(err, res) {
     test.ok(!err);
     test.equals(res.statusCode, 200, 'expecting 200');
     console.info('test1 done');
+    test.done();
+  });
+}
+
+exports.test2 = function(test) {
+  request(target(test) + '/bad', function(err, res) {
+    test.ok(!err);
+    test.equals(res.statusCode, 403, 'expecting 403');
+    console.info('test2 done');
     test.done();
   });
 }
