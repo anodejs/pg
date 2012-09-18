bf8672890ca6c6340758328bd9d3f67d53823dbe
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-18 11:25:03 +0300

    debug tests

--- yosefd/demo3/test/tests.js
+++ yosefd/demo3/test/tests.js
@@ -1,6 +1,10 @@
 var request = require('request');
 
+console.info('loading demo3 test');
+
 exports.test1 = function(test) {
+  console.info('running test1');
+
   var target = (test.context && text.context.host) || 'localhost:5000';
 
   console.info('target', target);
