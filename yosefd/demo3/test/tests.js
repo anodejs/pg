c1de0566fc120597678be5403ea7272b69349989
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-18 11:22:25 +0300

    debug tests

--- yosefd/demo3/test/tests.js
+++ yosefd/demo3/test/tests.js
@@ -3,8 +3,10 @@ var request = require('request');
 exports.test1 = function(test) {
   var target = (test.context && text.context.host) || 'localhost:5000';
 
+  console.info('target', target);
+
   request('http://' + target, function(err, res) {
-    test.ok(!err);
+    console.info('err:', err);
     test.equals(res.statusCode, 200, 'expecting 200');
     test.done();
   });
