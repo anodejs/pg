be1c05c6f32ea0db346805967ae7fa076a9ea086
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-19 18:07:30 +0300

    mmm

--- yosefd/demo4/index.js
+++ yosefd/demo4/index.js
@@ -6,3 +6,8 @@ srv.listen(process.env.PORT || 5000);
 srv.get('/', function(req, res) {
   res.send('Hello from anode demo4', 200);
 });
+
+srv.get('/mmm/?', function(req, res) {
+  console.warn('tried us');
+  res.send('not for your eyes', 403);
+});
