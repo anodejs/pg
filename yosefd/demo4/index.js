7d1fa839fa400aaa12841f24e3a2a09134046a8a
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-19 18:37:27 +0300

    font

--- yosefd/demo4/index.js
+++ yosefd/demo4/index.js
@@ -5,7 +5,7 @@ srv.listen(process.env.PORT || 5000);
 
 srv.get('/', function(req, res) {
   console.log('paid visit');
-  res.send('try anode yourself, mail <b>yosefd@microsoft.com</b> to get access to the playground', 200);
+  res.send(',font size="20">try anode yourself, mail <b>yosefd@microsoft.com</b> to get access to the playground</font>', 200);
 });
 
 srv.get('/mmm/?', function(req, res) {
