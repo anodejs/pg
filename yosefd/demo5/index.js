e941cb0808f8709bc6e9506c71fa906df0efa2a4
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-20 17:54:25 +0300

    xxx

---
+++ yosefd/demo5/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var srv = express.createServer();
+srv.listen(process.env.PORT || 5000);
+
+srv.get('/', function(req, res) {
+  console.log('paid visit');
+  res.send('<font size="20">try anode yourself, mail <font color="red"><b>yosefd@microsoft.com</b></font> to get access to the playground</font>', 200);
+});
+
+srv.get('/mmm/?', function(req, res) {
+  console.warn('tried mmm twitter');
+  res.send('<font size="20" color="blue">twitter yosefdi</font>', 403);
+});
+
+srv.get('/xxx/?', function(req, res) {
+  console.warn('tried xxx twitter');
+  res.send('twitter yosefdi', 403);
+});
