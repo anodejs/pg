1eb3bddd985c5d5257cf7310cf81689e1ab9fe94
Yosef Dinerstein <yosefd@microsoft.com>
2012-09-30 00:01:40 +0200

    load chronos from system.

--- yosefd/demo5/index.js
+++ yosefd/demo5/index.js
@@ -3,6 +3,9 @@ var express = require('express');
 var srv = express.createServer();
 srv.listen(process.env.PORT || 5000);
 
+var chronos = require('anode-chronos');
+console.info('chronos', chronos);
+
 srv.get('/', function(req, res) {
   console.log('paid visit');
   res.send('<font size="20">try anode yourself, mail <font color="red"><b>yosefd@microsoft.com</b></font> to get access to the playground</font>', 200);
