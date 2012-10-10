1acd499fbc98573582cdab95a64df29a47d4e96c
Yosef Dinerstein <yosefd@microsoft.com>
2012-10-10 19:34:58 +0200

    bla

--- giladeis/index.js
+++ giladeis/index.js
@@ -4,6 +4,11 @@ var srv = express.createServer();
 srv.listen(process.env.PORT || 5000);
 
 srv.get('/', function(req, res) {
-  console.info('got req', req);
+  console.info('got req');
   res.send('<font size="20">Gilad, please try anode yourself, mail <font color="red"><b>yosefd@microsoft.com</b></font> to get access to the playground</font>', 200);
+});
+
+srv.get('/bad/?', function(req, res) {
+  console.warn('bad request');
+  res.send('bad request', 403);
 });
