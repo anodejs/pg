96418a34fed05a2b36db24c9bc603c52f26f3d7e
Yosef Dinerstein <yosefd@microsoft.com>
2012-10-10 19:30:40 +0200

    new

---
+++ giladeis/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+var srv = express.createServer();
+srv.listen(process.env.PORT || 5000);
+
+srv.get('/', function(req, res) {
+  res.send('<font size="20">Gilad, please try anode yourself, mail <font color="red"><b>yosefd@microsoft.com</b></font> to get access to the playground</font>', 200);
+});
