9ffe2beceeae921ad74ef81d757f384d807d1458
Mousa Ziadeh <v-moziad@microsoft.com>
2012-10-21 12:43:13 +0200

    fix error

--- mousaz/demo2/index.js
+++ mousaz/demo2/index.js
@@ -5,7 +5,7 @@ var server = express.createServer();
 server.get("/", function (req, res) {
   console.info("request received");
   res.end("Hello PalDev");
-}):
+});
 
 server.listen(process.env.PORT || 5000);
 
