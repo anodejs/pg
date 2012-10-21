bbf611461788125107dc21eefffc7aee21956d42
Yosef Dinerstein <yosefd@microsoft.com>
2012-10-21 14:53:15 +0200

    upd

--- yosefd/demo7/index.js
+++ yosefd/demo7/index.js
@@ -4,5 +4,6 @@ var srv = express.createServer();
 srv.listen(process.env.PORT || 5000);
 
 srv.get('/', function(req, res) {
+  console.info('somebody interested!');
   res.send('Hi Rachel!!!', 200)
 });
