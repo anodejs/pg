f27e544bf1a7e800d387b18fc07e69916df814d9
Yosef Dinerstein <yosefd@microsoft.com>
2012-10-25 12:00:00 +0200

    new demo

---
+++ yosefd/demo8/index.js
@@ -0,0 +1,8 @@
+var experss = require('express');
+
+var srv = express.createServer();
+srv.listen(process.env.PORT || 5000);
+
+srv.get('/', function(req, res) {
+  res.send('<font size="20">try anode yourself, mail <font color="red"><b>yosefd@microsoft.com</b></font> to get access to the playground</font>', 200);
+});
